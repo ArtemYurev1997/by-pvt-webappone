076017442f0d5b1b50ce3bd075c87e10240370e4
artem <artem.yurev.1997@gmail.com>
2023-08-01 14:31:48 +0300

    Initial commit

---
+++ src/main/java/by/pvt/webappone/Web.java
@@ -0,0 +1,19 @@
+package by.pvt.webappone;
+
+import jakarta.servlet.GenericServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Web extends GenericServlet {
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        servletResponse.setContentType("application/json");
+        PrintWriter pw = servletResponse.getWriter();
+        pw.println("I like Java");
+        pw.close();
+    }
+}
